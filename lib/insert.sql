2e5387e8e4a3ec24729a0e867fa452a73ebd9931
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-06-02 22:04:30 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES('Mr. Chocolate', 12, 'M', 'brown', 'tame', 1);
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES('Rowdy', 6, 'M', 'white', 'wild', 1);
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES('Tabitha', 10, 'F', 'brown', 'wild', 0);
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES('Sergeant Brown', 8, 'M', 'black', 'tame', 1);
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES('Melissa', 11, 'M', 'black', 'tame', 1);
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES('Grinch', 9, 'F', 'black', 'wild', 1);
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES('Wendy', 4, 'F', 'brown', 'tame', 1);
+INSERT INTO bears (name,age,sex,color,temperament, alive) VALUES(NULL, NULL, 'M', 'brown', 'wild', 1);
